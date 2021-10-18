1f4a3cab98eb9098355bcdf1fd330a0ee0900f02
SharonFlorez <shdflorezs35@gmail.com>
2021-10-17 20:52:10 -0500

    Incorporacion del Register y del Login

--- app.js
+++ app.js
@@ -3,7 +3,17 @@ const path = require ('path');
 const app = express ();
 const port = process.env.PORT || 3000;
 app.use(express.static(path.resolve(__dirname,"./public")));
+
 app.get("/",(req,res)=>{
     res.sendFile(path.resolve(__dirname, "./views/home.html"))
 });
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/register.html'))
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/login.html'))
+}); 
+
 app.listen(port, () => console.log ('servidor corriendo en puesto 3000'));
